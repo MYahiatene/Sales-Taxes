2c104debfe84c421ba8f7a698d9100e3ed6944a1
Mokrane Yahiatene <myahiate@math.uni-bielefeld.de>
2022-04-20 17:14:56 +0200

    added frontend and backend structure

---
+++ src/it/java/de/myahiatene/salestaxes/SalestaxesApplicationTestsIT.java
@@ -0,0 +1,13 @@
+package de.myahiatene.salestaxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalestaxesApplicationTestsIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/de/myahiatene/salestaxes/SalestaxesApplication.java
@@ -0,0 +1,13 @@
+package de.myahiatene.salestaxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestaxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestaxesApplication.class, args);
+	}
+
+}
